9aa897dbe60215bcd127182c3532bd530aee1a14
Mario Muñoz <j.mario.msx@gmail.com>
2024-06-15 21:26:16 +0200

    added fade in component

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -18,11 +18,6 @@ export default {
       },
       animation: {
         pop: 'pop 0.6s ease-out',
-        'fade-in': 'fade-in 0.8s ease-in forwards',
-        'fade-in-up': 'fade-in-up 0.8s ease-in-out forwards',
-        'fade-in-right': 'fade-in-right 0.8s ease-in-out forwards',
-        'fade-in-left': 'fade-in-left 0.8s ease-in-out forwards',
-        'fade-in-down': 'fade-in-down 0.8s ease-in-out forwards',
       },
       keyframes: {
         pop: {
@@ -34,54 +29,6 @@ export default {
           },
           '100%': {
             transform: 'scale(1)',
-          },
-        },
-        'fade-in': {
-          '0%': {
-            opacity: '0',
-          },
-          '100%': {
-            opacity: '1',
-          },
-        },
-        'fade-in-up': {
-          '0%': {
-            opacity: '0',
-            transform: 'translateY(20px)',
-          },
-          '100%': {
-            opacity: '1',
-            transform: 'translateY(0)',
-          },
-        },
-        'fade-in-right': {
-          '0%': {
-            opacity: '0',
-            transform: 'translateX(-20px)',
-          },
-          '100%': {
-            opacity: '1',
-            transform: 'translateX(0)',
-          },
-        },
-        'fade-in-left': {
-          '0%': {
-            opacity: '0',
-            transform: 'translateX(20px)',
-          },
-          '100%': {
-            opacity: '1',
-            transform: 'translateX(0)',
-          },
-        },
-        'fade-in-down': {
-          '0%': {
-            opacity: '0',
-            transform: 'translateY(-20px)',
-          },
-          '100%': {
-            opacity: '1',
-            transform: 'translateY(0)',
           },
         },
       },
